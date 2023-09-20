6033b5bde6a84e15b98569161c55b74a993948ce
Gallillio <ahmedgalal11045@gmail.com>
2023-09-20 14:36:22 +0300

    SCROLLBARS ADDED FINALLYYY

--- Supervised_ML_Classifier_python.py
+++ Supervised_ML_Classifier_python.py
@@ -1,4 +1,4 @@
-#~~ Imports ~~#
+#Imports
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -26,9 +26,9 @@ from sklearn.model_selection import GridSearchCV
 
 #Global Variables that need to be placed
 root = Tk()
-file_path = 'change this in function below' #will be reassigned in file_path_opener()
+file_path = 'change this in function below' #will be reassigned in csv_opener()
 choose_csv_frame = LabelFrame(root, pady= 6, padx=15)
-df = pd.DataFrame() #will ve reassigned in file_path_opener()
+df = pd.DataFrame() #will ve reassigned in csv_opener()
 
 def create_root(project_name):
     
@@ -40,10 +40,11 @@ def create_root(project_name):
     title_label.place(anchor=CENTER, relx= 0.5, y=10)
     
     #size
-    width= root.winfo_screenwidth()               
-    height= root.winfo_screenheight()
-    root.geometry("%dx%d" % (width, height))
-    # root.geometry("500x500")
+    # width= root.winfo_screenwidth()               
+    # height= root.winfo_screenheight()
+    # root.geometry("%dx%d" % (width, height))
+    root.geometry("1400x700")
+    root.resizable(False,False)
 
 def root_loop() -> int:
     #start root loop
@@ -53,7 +54,7 @@ def root_loop() -> int:
 
  #get file path
 
-def file_path_opener():
+def csv_opener():
     #choose CSV File
     csv_file_name = Label(choose_csv_frame ,text="No File Selected")
     choose_csv_frame.filename = filedialog.askopenfilename(initialdir="/C", title="Select CSV File", filetypes=(("CSV Files", "*.csv"),))
@@ -71,40 +72,45 @@ def file_path_opener():
     if csv_submission_successful == "ok":
         choose_csv_frame.place_forget()
         treeview_of_df()
-    
-
 
 def choose_csv():
     #label frame to put things in
-    # choose_csv_frame.grid(row=1,column=0)
     choose_csv_frame.place(anchor=CENTER, relx= 0.5, y=50)
     
-    choose_csv_button = Button(choose_csv_frame, text="Choose Select CSV File", command= file_path_opener)
+    choose_csv_button = Button(choose_csv_frame, text="Choose Select CSV File", command= csv_opener)
     choose_csv_button.grid(row= 0, column=0)
-    # choose_csv_button.place(anchor=CENTER, relx= 0.5, y=50)
 
 def treeview_of_df():
     # create tree view frame & treeview
     treevew_data_frame = LabelFrame(root, padx=40, text="Data Display")
-    tree_scroll = Scrollbar(treevew_data_frame)
-    # tree_scroll.pack(side=RIGHT, fill=Y)
-    tv = ttk.Treeview(treevew_data_frame, height = 25)
+    tv = ttk.Treeview(treevew_data_frame,height=15)
+    y_scrollbar = ttk.Scrollbar(treevew_data_frame, orient="vertical")
+    x_scrollbar = ttk.Scrollbar(treevew_data_frame, orient="horizontal", command=tv.xview)
 
-    #put it on grid
-    # treevew_data_frame.grid(row= 1, column= 0)
-    treevew_data_frame.place(anchor=CENTER, relx= 0.5, rely= 0.3)
-    # tree_scroll.config(command= tv.yview)
+    #put frame on grid
+    treevew_data_frame.pack(fill=None, expand=False)
+    #put scroll bars
+    tv.configure(yscrollcommand=y_scrollbar.set, xscrollcommand=x_scrollbar.set)
 
+    y_scrollbar.pack(side=RIGHT, fill="y")
+    x_scrollbar.pack(side=BOTTOM, fill="x")
+    y_scrollbar.config(command= tv.yview)
+    x_scrollbar.config(command= tv.xview)
+
+    #show columns and list
     tv["column"] = list(df.columns)
     tv["show"] = "headings"
     for column in tv["column"]:
         tv.heading(column, text = column)
+        tv.column(column, width=100, minwidth=100)
 
     df_rows = df.to_numpy().tolist()
     for row in df_rows:
         tv.insert("", "end", values= row)
-    tv.grid(row= 100, column= 0)
-    # tv.place(anchor=CENTER, relx= 0.5, y=10)
+
+    # tv.place(width=646, height=410)
+    tv.pack()
+
     
 
 def main() -> int:
